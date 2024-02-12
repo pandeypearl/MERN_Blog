bc920768a6e6a89414aa28efdc25fc380646aa7e
Pretty Pandey <prettypandeypearl@gmail.com>
2024-02-12 16:32:52 +0200

    Client: Landing Page

---
+++ client/src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      
+      <header>
+        <a href='' className='logo'>Pretty's Blog</a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
+      </header>
+
+      <div className='post'>
+        <div className='image'>
+          <img src='https://images.unsplash.com/photo-1607457561901-e6ec3a6d16cf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        </div>
+        <div className='content'>
+          <h2>porttitor rhoncus dolor purus non enim praesent elementum facilisis leo</h2>
+          <p className='info'>
+            <a href='' className='author'>Pretty Pandey</a>
+            <time>12-02-2024 16:19</time>
+          </p>
+          <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+
+      <div className='post'>
+        <div className='image'>
+          <img src='https://images.unsplash.com/photo-1607457561901-e6ec3a6d16cf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        </div>
+        <div className='content'>
+          <h2>porttitor rhoncus dolor purus non enim praesent elementum facilisis leo</h2>
+          <p className='info'>
+            <a href='' className='author'>Pretty Pandey</a>
+            <time>12-02-2024 16:19</time>
+          </p>
+          <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+
+      <div className='post'>
+        <div className='image'>
+          <img src='https://images.unsplash.com/photo-1607457561901-e6ec3a6d16cf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        </div>
+        <div className='content'>
+          <h2>porttitor rhoncus dolor purus non enim praesent elementum facilisis leo</h2>
+          <p className='info'>
+            <a href='' className='author'>Pretty Pandey</a>
+            <time>12-02-2024 16:19</time>
+          </p>
+          <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+
+    </main>
+  );
+}
+
+export default App;
