8c702fcfa71422da68c47db71fb85dfae4ccb701
Pretty Pandey <prettypandeypearl@gmail.com>
2024-02-13 19:10:25 +0200

    API and Client: Conditional for logged in user, User Profile

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'adfjsdfhkadsfaksdfbdfdhf';
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 require('dotenv').config();
 const connectionString = process.env.MONGODB_URI;
@@ -42,6 +44,14 @@ app.post('/login', async (req, res) => {
   } else {
     res.status(400).json('Invalid credentials');
   }
+})
+
+app.get('/profile', (req, res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
 })
 
 app.listen(4000);
