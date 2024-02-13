7370c0cfdd03320a0139c58c82c494c41d50b848
Pretty Pandey <prettypandeypearl@gmail.com>
2024-02-13 18:36:06 +0200

    API: API setup and User Authentication

---
+++ api/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'adfjsdfhkadsfaksdfbdfdhf';
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(express.json());
+
+require('dotenv').config();
+const connectionString = process.env.MONGODB_URI;
+mongoose.connect(connectionString);
+
+app.post('/register', async (req, res) => {
+  const {username, password} = req.body;
+  try {
+    const userDoc = await User.create({
+      username, 
+      password:bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch(e) {
+    console.log(e);
+    res.status(400).json(e);
+  };
+});
+
+app.post('/login', async (req, res) => {
+  const {username, password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json('ok');
+    });
+  } else {
+    res.status(400).json('Invalid credentials');
+  }
+})
+
+app.listen(4000);
