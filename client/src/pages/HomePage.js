d86aebf78e44a8e166f85da4a7f28a6daa9d4d4c
Pretty Pandey <prettypandeypearl@gmail.com>
2024-02-17 11:18:52 +0200

    API: All Posts GET request endpoint

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import Post from '../components/Post';
 
 export default function HomePage() {
+	useEffect(() => {
+		fetch('/post').then(response => {
+			response.json().then(posts => {
+				console.log(posts);
+			})
+		})
+
+	}, []);
 	return (
 		<>
 			<Post />
