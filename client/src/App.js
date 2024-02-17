2917651a39d5fc86fa3993636cee81e13cf1bd40
Pretty Pandey <prettypandeypearl@gmail.com>
2024-02-17 17:11:20 +0200

    API and Client: Update Post Page and Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './utils/UserContext';
 import CreatePost from './pages/CreatePost';
 import PostDetailPage from './pages/PostDetailPage';
+import EditPostPage from './pages/EditPostPage';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/new-post' element={<CreatePost />} />
           <Route path='/post/:id' element={<PostDetailPage />} />
+          <Route path='/edit/:id' element={<EditPostPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
