4792ef4aba8064f95c08e19257bbe77da814616a
Pretty Pandey <prettypandeypearl@gmail.com>
2024-02-17 11:10:26 +0200

    Client: Redirect after New Created Post

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { Navigate } from 'react-router-dom';
 
 const modules = {
   toolbar: [
@@ -24,6 +25,7 @@ export default function CreatePost() {
   const [summary, setSummary] = useState('');
   const [content, setContent] = useState('');
   const [files, setFiles] = useState('');
+  const [redirect, setRedirect] = useState(false);
 
   async function createNewPost(ev) {
     const data = new FormData();
@@ -36,9 +38,14 @@ export default function CreatePost() {
       method: 'POST',
       body: data,
     });
-    console.log(await response.json());
+    if (response.ok) {
+      setRedirect(true);
+    }
   }
 
+  if (redirect) {
+    return <Navigate to={'/'}/>
+  }
   return (
     <form onSubmit={createNewPost}>
       <input 
