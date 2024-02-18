b50be0786b3bc261455d1566f9f6429ec06a872f
Pretty Pandey <prettypandeypearl@gmail.com>
2024-02-18 16:34:19 +0200

    Post Item Styling Adjustment

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -3,23 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function Post({_id, title, summary, cover, content, createdAt, author}) {
     return (
-      <div className='post'>
-        <div className='image'>
-          <Link to={`/post/${_id}`}>
-            <img src={'http://localhost:4000/'+cover} alt='' />
-          </Link>
-          
+      <Link className='post-link' to={`/post/${_id}`}>
+        <div className='post'>
+          <div className='image'>
+            <img src={'http://localhost:4000/'+cover} alt='Post Cover' />
+          </div>
+          <div className='content'>
+            <p className='info'>
+              <a href='' className='author'>@{author.username}</a>
+              <time>{formatISO9075(new Date(createdAt))}</time>
+            </p>
+            <h2>{title}</h2>
+            <p className='description'>{summary}</p>
+          </div>
         </div>
-      <div className='content'>
-        <Link to={`/post/${_id}`}>
-          <h2>{title}</h2>
-        </Link>
-        <p className='info'>
-          <a href='' className='author'>{author.username}</a>
-          <time>{formatISO9075(new Date(createdAt))}</time>
-        </p>
-        <p className='description'>{summary}</p>
-      </div>
-      </div>
+      </Link>
     ) 
 }
