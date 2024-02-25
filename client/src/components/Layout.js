487edec3355df2f67b57c303abe1402513e2adf9
Pretty Pandey <prettypandeypearl@gmail.com>
2024-02-25 21:11:26 +0200

    Client: Adding ToggleButton to Layout component

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,11 +1,27 @@
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
+import ToggleButton from './ToggleButton';
+import { useLightDarkMode } from '../utils/LightDarkModeContext';
+import { useEffect } from 'react';
 
 export default function Layout () {
+  const { isLightMode } = useLightDarkMode();
+
+  useEffect(() => {
+    if (isLightMode) {
+      document.body.classList.remove('dark-mode');
+      document.body.classList.add('light-mode');
+    } else {
+      document.body.classList.remove('light-mode');
+      document.body.classList.add('dark-mode');
+    }
+  }, [isLightMode]);
+
   return (
     <main>
+      <ToggleButton />
       <Header />
       <Outlet />
     </main>
-  )
+  );
 }
--- client/src/components/ToggleButton.js
+++ client/src/components/ToggleButton.js
@@ -3,10 +3,14 @@ import { useLightDarkMode } from "../utils/LightDarkModeContext";
 export default function ToggleButton() {
     const { isLightMode, toggleMode } = useLightDarkMode();
 
+    const handleToggle = () => {
+        toggleMode(); // Call the toggle function to update the mode state
+    };
+
     return (
         <div className='toggle-wrapper'>
             <label className='toggle-btn'>
-                <input type='checkbox' checked={isLightMode} onChange={toggleMode} />
+                <input type='checkbox' checked={isLightMode} onChange={handleToggle} />
                 <span className='toggle-btn-label'>
                     <span className='toggle-btn-handle'></span>
                 </span>
