55691d9d3c36558d61fccc39dc2a0fdd7700559b
Pretty Pandey <prettypandeypearl@gmail.com>
2024-03-05 17:50:39 +0200

    Client: About Page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import AboutPage from './pages/AboutPage';
 import { UserContextProvider } from './utils/UserContext';
 import CreatePost from './pages/CreatePost';
 import PostDetailPage from './pages/PostDetailPage';
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
             <Route path='/login' element={<LoginPage />} />
             <Route path='/register' element={<RegisterPage />} />
             <Route path='/new-post' element={<CreatePost />} />
@@ -25,7 +27,9 @@ function App() {
               <Route path='share/:shareId' element={<PostDetailPage />} />
             </Route>
             <Route path='/edit/:id' element={<EditPostPage />} />
+            
           </Route>
+          
         </Routes>
       </LightDarkModeProvider>
     </UserContextProvider>
---
+++ client/src/pages/AboutPage.js
@@ -0,0 +1,52 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import {  faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import profileImage from '../assets/images/Pretty_Pandey_Profile.webp';
+
+export default function AboutPage() {
+    return (
+        <div className='about-wrapper'>
+            <div className='profile'>
+                <img src={profileImage} alt='Profile'/>
+                <ul>
+                    <li>
+                        <a href=''>
+                            <FontAwesomeIcon icon={faEnvelope} color='#9D68FF' />Get In Touch
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://www.prettypandey.tech' target='_blank' rel="noreferrer">
+                            <FontAwesomeIcon icon={faAddressCard} color='#9D68FF' />Portfolio Website
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://www.twitter.com/prettyppandey' target='_blank' rel="noreferrer">
+                            <FontAwesomeIcon icon={faXTwitter} color='#9D68FF' />Follow Me On Twitter/X
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className='text'>
+                <p>Hello World! Welcome to PrettyTech.</p>
+                <p>I'm Pretty Pandey, the face behind this tech blog. Thanks for stopping by!</p>
+                <p>As a full-stack developer, I spend my days diving into lines of code, building projects, and exploring the latest trends in the tech world.
+                    PrettyTech is my way of sharing what I've learned and experienced along the way.
+                    Whether it's building a slick new web app or tinkering with cutting-edge AI, you'll get an inside look at the creative process behind the code.
+                </p>
+                <p>Expect to hear stories about my own projects – the highs, the lows, and everything in between.
+                    And if you're working on something exciting too, I'd love to hear about it! Please feel free to get in touch.
+                </p>
+                <p>But this blog isn't just about my adventures in coding.
+                    It's also a place to level up your skills with tutorials and insights covering everything from backend magic to frontend wizardry.
+                </p>
+                <p>So whether you're a seasoned developer looking for a different perspective or a newcomer eager to learn the ropes,
+                    I invite you to join me on this journey through the wonderful world of technology.
+                    Together, we'll explore, learn, and grow as we unlock the endless possibilities that await us in the world of code.
+                </p>
+                <p>So, grab a cup of your favorite beverage, take a seat, and let's explore the fascinating world of tech together.
+                    Thanks for being part of this journey!</p>
+            </div>
+        </div>
+    );
+}
