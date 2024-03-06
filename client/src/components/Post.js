cc6ba8c580dfbc8aab1428cea4611251e3d291f2
Pretty Pandey <prettypandeypearl@gmail.com>
2024-03-06 20:54:07 +0200

    Client: Updating Home Page Layout

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,5 +1,7 @@
 import {formatISO9075} from 'date-fns';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 export default function Post({_id, title, summary, cover, content, createdAt, author, viewCount, sharableLink}) {
     return (
@@ -9,12 +11,15 @@ export default function Post({_id, title, summary, cover, content, createdAt, au
             <img src={'http://localhost:4000/'+cover} alt='Post Cover' />
           </div>
           <div className='content'>
-            <p className='info'>
-              <a href='' className='author'>@{author.username}</a>
-              <time>{formatISO9075(new Date(createdAt))}</time>
-            </p>
-            <h2>{title}</h2>
-            <p className='description'>{summary}</p>
+            <div>
+              <h2>{title}</h2>
+              <p className='info'>
+                <time>{formatISO9075(new Date(createdAt))}</time>
+                <span className='author'>@{author.username}</span>
+              </p>
+              <p className='description'>{summary}</p>
+            </div>
+            <span className='post-views'><FontAwesomeIcon icon={faEye} color='#9D68FF' /> {viewCount}</span>
           </div>
         </div>
       </Link>
